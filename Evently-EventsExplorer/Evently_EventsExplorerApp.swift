8c8dd7e31388c85714965e693574da49fa2e78cc
Nanda WK <nandawinkyu.ix@gmail.com>
2025-07-22 15:38:39 +0700

    Initial Commit

---
+++ Evently-EventsExplorer/Evently_EventsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evently_EventsExplorerApp.swift
+//  Evently-EventsExplorer
+//
+//  Created by Nanda WK on 2025-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Evently_EventsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
