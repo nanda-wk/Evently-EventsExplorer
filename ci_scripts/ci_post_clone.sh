d05d0100b9103ed6f89702b5333985fcb36ce263
Nanda WK <nandawinkyu.ix@gmail.com>
2025-09-04 00:35:28 +0700

    fix: correct API endpoint

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Evently-EventsExplorer
+#
+#  Created by Nanda WK on 2025-09-04.
+#
+
+echo "API_BASE_URL = $API_BASE_URL" >> /Users/nanda-wk/Workspace/Xcode_workspace/Evently-EventsExplorer/Evently-EventsExplorer/Resources/Env.xcconfig
+echo "API_KEY = $API_KEY" >> /Users/nanda-wk/Workspace/Xcode_workspace/Evently-EventsExplorer/Evently-EventsExplorer/Resources/Env.xcconfig
